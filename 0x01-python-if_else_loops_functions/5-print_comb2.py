03cd304a061cd32989b7c5b3e375d861fd117740
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 09:46:07 +0000

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # A program that prints numbers from 0-99
-for i in range(10):
-    for j in range(10):
-        if (i == 9 and j == 9):
-            print("{}{}".format(i, j), end='\n')
+for number in range(100):
+    print("{}".format(number), end=', ')
