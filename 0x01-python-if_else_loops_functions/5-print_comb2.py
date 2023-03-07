b3547117e2d1e1900c3855010beeea1c5c39159e
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 09:42:09 +0000

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,5 +4,3 @@ for i in range(10):
     for j in range(10):
         if (i == 9 and j == 9):
             print("{}{}".format(i, j), end='\n')
-        else:
-            print("{}{}".format(i, j), end=', ')
