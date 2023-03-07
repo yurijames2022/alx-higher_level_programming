42369307f616a08f322247fc56e1e3bda7097661
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 08:02:18 +0000

    print alphabt

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# A program that prints alphabet except q and e
+
+for alphabet in range(97, 123):
+    if alphabet != 101 and alphabet != 113:
+        print("{}".format(chr(alphabet)), end = '')
