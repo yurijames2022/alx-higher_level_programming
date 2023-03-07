ce94535b0d43892890e7b51556b23f48b27c785c
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 09:19:56 +0000

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 # A program that prints numbers from 0-99
 for number in range(100):
     if number != 99:
-        print("{0:02d}".format(number), end = ', ')
+        print("{0:02d}".format(number), end=', ')
     else:
         print("{0:02d}".format(number))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(10):
             if ((i == 8 and j == 9)):
                 print("{}{}".format(i, j))
             else:
-                print("{}{}".format(i,j), end = ', ')
+                print("{}{}".format(i,j), end=', ')
