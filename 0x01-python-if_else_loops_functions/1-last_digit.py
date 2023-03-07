fd5eecf513df8bce9c379250c7f5f2e6bd82c5bb
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 08:37:27 +0000

    remove newline

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ last_digit_str = number_to_str[-1]
 last_digit = int(last_digit_str)
 
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5\n".format(number, last_digit))
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 
 elif last_digit == 0:
-    print("Last digit of {} is {} and is zero\n".format(number, last_digit))
+    print("Last digit of {} is {} and is zero".format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0\n".format(number, last_digit))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# A program that prints numbers from 0-99
+for number in range(100):
+    if number != 99:
+        print("{0:02d}".format(number), end = ', ')
+    else:
+        print("{0:02d}".format(number))
