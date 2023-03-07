3d27dd9711c4bdab88b02404c45e98690a1554b9
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 09:15:25 +0000

    corrections

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # A python program that prints the alphabet
 for alphabet in range(97, 123):
-    print('{}'.format(chr(alphabet)), end = "")
+    print('{}'.format(chr(alphabet)), end="")
