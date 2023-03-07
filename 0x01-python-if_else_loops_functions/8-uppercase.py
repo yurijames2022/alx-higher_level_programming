cceab8c1d6204b2b323231ae03e98978a8357460
Jack Yuri <yurijamesmail@gmail.com>
2023-03-07 11:35:00 +0000

    Print last digit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# A function that converts lowercase to uppercase
+def uppercase(str):
+    
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# A function that prints the last digit of a number
+def print_last_digit(number):
+    number_str = str(number)
+    last_digit = number_str[-1]
+    return last_digit
