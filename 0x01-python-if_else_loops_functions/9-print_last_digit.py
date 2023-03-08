18268971efaabeaaf195bee4335a4308c074c796
Jack Yuri <yurijamesmail@gmail.com>
2023-03-08 10:08:52 +0000

    Last digit fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # A function that prints the last digit of a number
 def print_last_digit(number):
+    number = abs(number)
     last_digit = number % 10
+    print('{}'.format(last_digit), end='')
     return last_digit
