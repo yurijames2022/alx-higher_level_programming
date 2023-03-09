a74333c2bb3c3bfc5d825c84c75c4c5be8abd20a
Jack Yuri <yurijamesmail@gmail.com>
2023-03-09 12:21:28 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-del sys.argv[0]
-number = 0
-len_args = len(sys.argv)
 
-if len_args == 1:
-    print("{} argument:".format(len_args))
-    print("{}: {}".format(len_args, sys.argv[0]))
+len_args = len(sys.argv) - 1
+
 if len_args == 0:
     print("{} arguments.".format(len_args))
-if len_args != 0 and len_args != 1:
+elif len_args == 1:
+    print("{} argument:".format(len_args))
+else:
     print("{} arguments:".format(len_args))
-    for i in sys.argv:
-        number += 1
-        print("{}: {}".format(number, i))
+
+if len_args > 0:
+    for i in range(len_args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 # A program that prints the result of the addition of all arguments
-del sys.argv[0]
-
-store = 0
-for i in sys.argv:
-    i = int(i)
-    store += i
-print(store)
+len_args = len(sys.argv)
+result = 0
+j = 0
+for i in range(len_args):
+    if i != sys.argv[0]:
+        j = int(sys.argv[i])
+        result += j
+print("{}".format(result))
