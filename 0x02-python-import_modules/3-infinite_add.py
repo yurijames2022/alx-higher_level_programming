1a89a15da2970b10781ad16709e762a7094726b8
Jack Yuri <yurijamesmail@gmail.com>
2023-03-09 11:49:10 +0000

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
 # A program that prints the result of the addition of all arguments
+del sys.argv[0]
+
 store = 0
 for i in sys.argv:
+    i = int(i)
     store += i
 print(store)
