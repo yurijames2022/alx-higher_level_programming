bf837089a943f1bffd8e2286bd05abed6d27b4e4
Jack Yuri <yurijamesmail@gmail.com>
2023-03-09 09:37:34 +0000

    add fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-# A module that imports another module for addition purpose
-import add_0
+import sys
+if __name__ == '__main__':
+    from add_0 import add
+else:
+    sys.exit()
+
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
