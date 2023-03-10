19795c748d7ee323267f44d470c1c8aa48bccd0a
Jack Yuri <yurijamesmail@gmail.com>
2023-03-10 08:22:34 +0000

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# A function that replaces an element of a list at specific position(like in C)
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
