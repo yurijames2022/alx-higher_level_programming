ec0eb55330451aeff104362b997402f2c12e1704
Jack Yuri <yurijamesmail@gmail.com>
2023-03-27 09:31:53 +0000

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end='' if i < (x - 1) else '\n')
             i += 1
-    except (IndexError, NameError, ValueError:
+    except (IndexError, NameError, ValueError, TypeError):
         print()
         pass
     return i
