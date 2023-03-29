e1ef50f024f6a4dd611f83ab91c9fbaf34769831
Jack Yuri <yurijamesmail@gmail.com>
2023-03-29 09:01:51 +0000

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' Creating a class that defines a square '''
+
+
+class Square:
+    def __init__(self, size):
+        '''
+        Args:
+             size: The size of the square
+        '''
+        self.__size = size
