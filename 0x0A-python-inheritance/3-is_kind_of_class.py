2a0d9a573812d15d5b1226fdc0a22f721b11beae
James Yuri <james@jack>
2023-04-11 16:54:17 +0300

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+''' A function that checks for instance '''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' Function that checks for instance
+    Args:
+    obj: The object
+    a_class: The class
+
+    Returns: Returns True if isinstance. False if not
+    '''
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
