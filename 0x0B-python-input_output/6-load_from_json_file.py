7a1d3fbf2bfcfb8b9b8b2416f69ae882f542fc15
James Yuri <james@jack>
2023-04-12 15:28:12 +0300

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A function that creates an object from a JSON file """
+import json
+
+
+def load_from_json_file(filename):
+    """ A function that creates an object from a JSON file
+
+    Args:
+    filename (str): The file destination
+
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+       obj_file = json.load(f)
+       return obj_file
