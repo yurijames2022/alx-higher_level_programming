2e64732050ad9b1bba5eb51964f018359ce2f7de
James Yuri <james@jack>
2023-04-12 15:02:52 +0300

    5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A function that writes an Object to a text file using JSON rep. """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ A function that writes an object to a text file.
+
+    Args:
+    my_obj: The object.
+    filename (str): The file destination.
+
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
