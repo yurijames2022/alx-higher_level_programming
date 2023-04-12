de8a39fd35998723caff02ab72175e53c94b1734
James Yuri <james@jack>
2023-04-12 12:40:10 +0300

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A function that reads a text file """
+
+
+def read_file(filename=""):
+    """ A function that reads a text file and prints it to stdout.
+
+    Args:
+    filename (str): The file destination of the file to be read.
+
+    """
+
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read())
