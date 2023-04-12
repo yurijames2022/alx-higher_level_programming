c70b249e757d7977606965fac191e51d964398b9
James Yuri <james@jack>
2023-04-12 12:44:15 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,5 @@ def read_file(filename=""):
 
     """
 
-    with open(filename, encoding="utf-8") as my_file:
+    with open((filename), "r", encoding="utf-8") as my_file:
         print(my_file.read())
