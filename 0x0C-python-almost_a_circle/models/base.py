856cc77ebf99e32175fbd4f8d4286101dfd666fa
james <yurijamesmail@gmail.com>
2023-04-19 12:33:37 +0000

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A class named base
+"""
+
+
+class Base():
+    """ A class named Base """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ The initializer
+
+        Args:
+        id (int): an id argument
+
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
