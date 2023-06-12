efd7b59aa13879192e5b425c2869d43b4d4f6d63
James Yuri <yurijamesmail@gmail.com>
2023-06-12 13:07:55 +0300

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const listArgs = process.argv;
 if (listArgs.length === 2) {
-  console.log('No Arguments');
+  console.log('No argument');
 } else if (listArgs.length === 3) {
   console.log('Argument found');
 } else if (listArgs.length > 3) {
