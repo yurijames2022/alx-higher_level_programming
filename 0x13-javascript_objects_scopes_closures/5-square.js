ead6bbcc37544a9085ad821c556da2316356cb52
James Yuri <yurijamesmail@gmail.com>
2023-06-13 13:10:47 +0300

    5-fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,17 +4,5 @@ const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
-  }
-
-  print () {
-    const x = 'X'.repeat(this.size);
-    for (let i = 0; i < this.size; i++) {
-      console.log(x);
-    }
-  }
-
-  double () {
-    this.size = (this.size * 2);
   }
 };
